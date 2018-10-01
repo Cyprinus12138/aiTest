98309e7d259252ee66df8b633e91a30d24d17cdc
Cyprinus <Cyprinus12138@gmail.com>
2018-10-01 23:36:23 +0800

    Fix the Decoder

--- MNIST-Decoder/ubytedecoder.py
+++ MNIST-Decoder/ubytedecoder.py
@@ -7,11 +7,13 @@ class MNISTDecoder:
         self.fmt_1 = ">2i"
         self.fmt_3 = ">4i"
         self.bin_content = open(ubyte_file, 'rb').read()
-        file_extension = ubyte_file.split('.')[-1]
-        if file_extension == 'idx1-ubyte':
-            self.idx1()
-        elif file_extension == 'idx3-ubyte':
-            self.idx3()
+        self.file_extension = ubyte_file.split('.')[-1]
+
+    def decode_to_matrix(self):
+        if self.file_extension == 'idx1-ubyte':
+            return self.idx1()
+        elif self.file_extension == 'idx3-ubyte':
+            return self.idx3()
         else:
             raise TypeError("Unavailable file type")
 
@@ -22,7 +24,7 @@ class MNISTDecoder:
         fmt_label = '>B'
         labels = np.empty(number_of_items)
         for i in range(number_of_items):
-            labels[i] = struct.unpack_from(fmt_label, self.bin_content, offset)
+            labels[i] = struct.unpack_from(fmt_label, self.bin_content, offset)[0]
             offset += struct.calcsize(fmt_label)
         return labels
 
@@ -32,13 +34,9 @@ class MNISTDecoder:
         offset += struct.calcsize(self.fmt_3)
         image_size = rows * columns
         fmt_image = '>' + str(image_size) + 'B'
-        images = np.empty(number_of_images)
+        images = np.empty((number_of_images, rows, columns))
         for i in range(number_of_images):
             image = np.array(struct.unpack_from(fmt_image, self.bin_content, offset)).reshape((rows, columns))
             offset += struct.calcsize(fmt_image)
             images[i] = image
         return images
-
-
-
-
