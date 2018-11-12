4686eb478ee5f428ba8e0a1c05be3d492a1ca41f
Cyprinus <Cyprinus12138@gmail.com>
2018-11-13 00:54:38 +0800

    Complete the AdaBoost,add the classify threshold arg.

--- UCI/AdaBoost.py
+++ UCI/AdaBoost.py
@@ -1,7 +1,7 @@
 from UCI.Extension.Weaker import *
 from UCI.Extension.Decoder import *
 
-WEAKER_NUM = 10
+WEAKER_NUM = 6
 
 
 class AdaBoost:
@@ -14,14 +14,15 @@ class AdaBoost:
         for i in range(weaker_num):
             self.weaker_set.append(Weaker(0.1, dataset.shape))
         self.weaker_set[0].train_with_batch(x, y, 580)
+        self.weaker_set[0].get_alpha()
 
-    def inference(self, x):
-        result = sum([weaker.alpha * self.inf_as_one(weaker.classify(x)) for weaker in self.weaker_set])
-        return self.inf_as_one(result)
+    def inference(self, x, threshold):
+        result = sum([weaker.alpha * self.inf_as_one(weaker.classify(x), 0) for weaker in self.weaker_set])
+        return self.inf_as_one(result, threshold)
 
     @staticmethod
-    def inf_as_one(y):
-        if y > 0:
+    def inf_as_one(y, m):
+        if y > m:
             return 1
         else:
             return -1
@@ -39,5 +40,15 @@ class AdaBoost:
             y = [sample for sample in self.weaker_set[i - 1].false_y] + [sample for sample in
                                                                          self.weaker_set[i - 1].true_y]
             self.weaker_set[i].train_with_batch(x, y, 580)
-        self.weaker_set[self.weaker_num - 1].get_alpha()
+            self.weaker_set[i].get_alpha()
 
+    def run_evaluation(self):
+        test_set = Decoder("./DataSet/1year.arff")
+        x, y = test_set.get_data(50000)
+        for threshold1 in range(1, 10):
+            threshold = 2.5 / threshold1
+            true_num = 0
+            for i in range(len(x)):
+                if self.inference(x[i], threshold) * y[i] > 0:
+                    true_num += 1
+            print(true_num / len(x))
