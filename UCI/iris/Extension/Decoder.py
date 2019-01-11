fb816f7a725d32ad37b0a98e8b291d07b1afaa06
Cyprinus <Cyprinus12138@gmail.com>
2019-01-11 15:54:26 +0800

    Complete Decoder.

---
+++ UCI/iris/Extension/Decoder.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+type_map = {"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2}
+
+
+class Decoder:
+    def __init__(self, file_path):
+        self.file = open(file_path, 'r')
+        self.shape = 0
+
+    def get_data(self, size):
+        x_list = []
+        y_list = []
+        for i in range(size):
+            line = self.file.readline()
+            x = line.split(",")
+            if len(x) <= 2:
+                return x_list, y_list
+            self.shape = len(x)-1
+            y = type_map[x[-1]]
+            x = [float(data) for data in x[:-1]]
+            x = [x[i] / np.sum(x[:-1]) for i in range(len(x) - 1)]
+            x = np.array(x)
+            x = x.reshape([np.shape(x)[0]])
+            x_list.append(x)
+            y_list.append(y)
+        return x_list, y_list
